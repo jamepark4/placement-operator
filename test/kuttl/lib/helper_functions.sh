9bfa3850341b437b2327dd6cab7f9f50f4a98bbc
James Parker <jparker@redhat.com>
2023-02-22 15:30:15 -0500

    Update fail message for regex function
    Add log message when regex fails and update how command calls the helper
    function to use bash -c 'cmd'.

--- test/kuttl/lib/helper_functions.sh
+++ test/kuttl/lib/helper_functions.sh
@@ -3,13 +3,18 @@
 assert_regex() {
 
     apiEndpoints=$(oc get -n openstack PlacementAPI  placement -o go-template-file="$1")
-    echo $apiEndpoints
-    matches=$(echo "$apiEndpoints" | sed -e "s?$2??")
-    if [ -z "$matches" ]; then
+    if [[ $apiEndpoints =~ $2 ]]; then
         exit 0
     else
+        printf '%s\n' "Regex check $2 failed against: $apiEndpoints";
         exit 1
     fi
 }
 
 "$@"
+
+adddate() {
+    while IFS= read -r line; do
+        printf '%s %s\n' "$(date)" "$line";
+    done
+}
