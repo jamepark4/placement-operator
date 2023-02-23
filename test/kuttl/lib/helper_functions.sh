f8c61ba411f9fd790e302a823fb935afbabf39ac
James Parker <jparker@redhat.com>
2023-02-23 15:56:58 -0500

    Update assert_regex to be more generic
    Update assert_regex so it can accept different template/oc output.

--- test/kuttl/lib/helper_functions.sh
+++ test/kuttl/lib/helper_functions.sh
@@ -2,19 +2,12 @@
 
 assert_regex() {
 
-    apiEndpoints=$(oc get -n openstack PlacementAPI  placement -o go-template-file="$1")
-    if [[ $apiEndpoints =~ $2 ]]; then
+    if [[ $1 =~ $2 ]]; then
         exit 0
     else
-        printf '%s\n' "Regex check $2 failed against: $apiEndpoints";
+        printf '%s\n' "Regex check '$2' failed against oc output: $1";
         exit 1
     fi
 }
 
 "$@"
-
-adddate() {
-    while IFS= read -r line; do
-        printf '%s %s\n' "$(date)" "$line";
-    done
-}
